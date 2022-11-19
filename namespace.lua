e12dd70780a6335bb146d7282e36442d054156f1
Deviatt <70473864+Deviatt@users.noreply.github.com>
2022-11-19 14:45:33 +0300

    Fix multidimensional namespaces

--- namespace.lua
+++ namespace.lua
@@ -5,13 +5,11 @@ local envStack = {}
 
 local setfenv, getfenv, setmetatable, pop = setfenv, getfenv, setmetatable, table.remove
 return function(ns)
-	if (not ns) then return setfenv(#envStack - 1, pop(envStack, #envStack)) end
-	local stackLen = #envStack
-	local env = getfenv(stackLen)
-	stackLen = stackLen + 1
-	envStack[stackLen] = env
+	if (not ns) then return setfenv(1, pop(envStack)) end
+	local env = getfenv(2)
+	envStack[#envStack + 1] = env
 
-	local namespace = env[ns] or setmetatable({}, {__index = envStack[stackLen - 1] or _G})
+	local namespace = env[ns] or setmetatable({}, {__index = envStack[#envStack] or _G})
 	env[ns] = namespace
-	setfenv(stackLen, namespace)
+	setfenv(2, namespace)
 end
