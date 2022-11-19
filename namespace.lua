e3606d4ca56bd3e9b3eb5877fa7f2c1b73e0896a
Deviatt <70473864+Deviatt@users.noreply.github.com>
2022-11-19 15:25:33 +0300

    Fix loop in gettable & possible loop

--- namespace.lua
+++ namespace.lua
@@ -3,13 +3,15 @@ assert(tonumber(_VERSION:sub(5, 7)) < 5.3, "Module not supported in Lua 5.3 or h
 
 local envStack = {}
 
-local setfenv, getfenv, setmetatable, pop = setfenv, getfenv, setmetatable, table.remove
+local setfenv, getfenv, setmetatable, getmetatable, pop = setfenv, getfenv, setmetatable, getmetatable, table.remove
 return function(ns)
 	if (not ns) then return setfenv(1, pop(envStack)) end
-	local env = getfenv(2)
+	local env = getfenv(1)
 	envStack[#envStack + 1] = env
 
-	local namespace = env[ns] or setmetatable({}, {__index = envStack[#envStack] or _G})
+	local meta = getmetatable(env[ns]) or {}
+	if (envStack[#envStack] ~= env[ns]) then meta.__index = envStack[#envStack] or _G end -- Fix 'loop in gettable' & 'possible loop'
+	local namespace = setmetatable(env[ns] or {}, meta)
 	env[ns] = namespace
-	setfenv(2, namespace)
+	setfenv(1, namespace)
 end
